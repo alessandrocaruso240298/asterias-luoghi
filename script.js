d4be3c8e51511889b554c5b91cbcdb3b0551522b
alessandrocaruso240298 <crslsn03h03m208e@studenti.unical.it>
2025-03-30 21:12:41 +0200

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function openModal(imgElement) {
-    // Creazione della finestra modale
     var modal = document.createElement('div');
     modal.style.position = 'fixed';
     modal.style.top = '0';
@@ -12,22 +11,19 @@ function openModal(imgElement) {
     modal.style.justifyContent = 'center';
     modal.style.zIndex = '1000';
 
-    // Creazione dell'immagine ingrandita
     var modalImg = document.createElement('img');
     modalImg.src = imgElement.src;
-    modalImg.style.width = '90vw'; // Larghezza adattabile
-    modalImg.style.height = 'auto'; // Mantiene le proporzioni
-    modalImg.style.maxHeight = '90vh'; // Non supera l'altezza dello schermo
+    modalImg.style.width = '90vw';
+    modalImg.style.height = 'auto';
+    modalImg.style.maxHeight = '90vh';
     modalImg.style.borderRadius = '10px';
     modalImg.style.boxShadow = '0px 0px 15px rgba(255,255,255,0.5)';
     modalImg.style.objectFit = 'contain';
 
-    // Chiudere la modale al tocco su mobile o clic su desktop
     modal.addEventListener('click', function () {
         document.body.removeChild(modal);
     });
 
-    // Aggiunta dell'immagine alla finestra modale
     modal.appendChild(modalImg);
     document.body.appendChild(modal);
 }
